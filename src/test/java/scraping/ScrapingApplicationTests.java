3d25a53a4596927240961893a57d92926fdeb1ca
Xu, Ling <ling.xu2@nttdata.com>
2020-04-15 22:41:38 +0200

    initial commit

---
+++ src/test/java/scraping/ScrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package scraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
